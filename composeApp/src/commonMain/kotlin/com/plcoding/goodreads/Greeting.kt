e7a06011ec7b96555aad19b577a9aa71785058a7
dipinsonwani <dipinsonwani@gmail.com>
2026-01-04 14:28:47 +0530

    feat: add search bar ui

--- composeApp/src/commonMain/kotlin/com/plcoding/goodreads/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.plcoding.goodreads
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/plcoding/goodreads/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/goodreads/book/presentation/book_list/BookListScreen.kt
@@ -1,33 +1,63 @@
 package com.plcoding.goodreads.book.presentation.book_list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.plcoding.goodreads.book.domain.Book
+import com.plcoding.goodreads.book.presentation.book_list.components.BookSearchBar
+import com.plcoding.goodreads.core.presentation.DarkBlue
 
 @Composable
 fun BookListScreen(
     viewModel: BookListViewModel,
     onBookClick: (Book) -> Unit,
-    modifier: Modifier ){
-
+) {
     val state by viewModel.state.collectAsStateWithLifecycle()
-    
-   BookListContent(state = state, onAction = { action ->
-       when(action){
-           is BookListAction.OnBookClick -> onBookClick(action.book)
-           else -> Unit
-       }
-   })
-}
 
+    BookListContent(state = state, onAction = viewModel::onAction)
+}
 
 
 @Composable
 private fun BookListContent(
     state: BookListState,
-    onAction: (BookListAction) -> Unit,){
+    onAction: (BookListAction) -> Unit,
+) {
 
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DarkBlue)
+            .statusBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        BookSearchBar(
+            searchQuery = state.searchQuery,
+            onSearchQueryChange = {
+                onAction(BookListAction.OnSearchQueryChange(it))
+            },
+            onImeSearch = {
+                keyboardController?.hide()
+            },
+            modifier = Modifier
+                .widthIn(max = 400.dp)
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
 
 }
